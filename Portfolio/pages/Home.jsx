74ef117b304a42deae5d8789037cd0d15c4a0855
Jay <jaymanning123@gmail.com>
2025-12-15 14:49:05 +0000

    Refactor portfolio structure: remove unused styles and implement Home component

---
+++ Portfolio/pages/Home.jsx
@@ -0,0 +1,25 @@
+export const Home = () => {
+    return (
+        <div>
+            <header>
+                <div className="container">
+                    <h1>Welcome To my Portfolio</h1>
+                </div>
+            </header>
+
+            <main>
+                <div className="container">
+                    <p>
+                    Hello! I'm Jay Manning, a passionate software developer with a knack for creating dynamic and user-friendly web applications. With a strong foundation in JavaScript, React, and Node.js, I enjoy building projects that solve real-world problems and enhance user experiences.
+                    </p>
+                    <p>
+                    In my portfolio, you'll find a selection of my work that showcases my skills in front-end and back-end development, as well as my ability to work with various technologies and frameworks. I'm always eager to learn new things and take on exciting challenges in the tech world.
+                    </p>
+                    <p>
+                    Feel free to explore my projects, and don't hesitate to reach out if you'd like to collaborate or learn more about my work!
+                    </p>
+                </div>
+            </main>
+        </div>
+    );
+};
